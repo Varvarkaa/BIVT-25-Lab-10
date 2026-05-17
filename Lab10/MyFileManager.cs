b8c4d18726b06891fdfa6d66c67f09c33043ea0f
Varvarkaa <tokaya.vi@mail.ru>
2026-05-17 23:02:51 +0300

    Add files via upload

---
+++ Lab10/MyFileManager.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10.Purple
+{
+    public abstract class MyFileManager : IFileManager, IFileLifeController
+    {
+
+        private string _name;
+        private string _filename;
+        private string _folderpath;
+        private string _fileextension;
+        private string _fileformat;
+
+        public string FullPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_fileextension))
+                {
+                    return Path.Combine(FolderPath, FileName);
+                }
+                else
+                {
+                    return Path.Combine(FolderPath, $"{FileName}.{_fileextension}");
+                }
+
+            }
+        }
+
+        public string Name => _name;
+        public string FolderPath => _folderpath;
+
+        public string FileExtension => _fileextension;
+
+        public string FileName => _filename;
+
+        public MyFileManager(string name)
+        {
+            _name = name;
+            _folderpath = "";
+            _fileextension = "";
+            _filename = "";
+            _fileformat = "";
+        }
+        public MyFileManager(string name, string folder, string file, string extension = "")
+        {
+            _name = name;
+            _folderpath = folder;
+            _filename = file;
+            _fileextension = extension;
+
+        }
+        public virtual void ChangeFileExtension(string newExt)
+        {
+            if (File.Exists(FullPath))
+            {
+
+                string newpath = Path.Combine(FolderPath, $"{FileName}.{newExt}");
+
+                File.Move(FullPath, newpath);
+            }
+            _fileextension = newExt;
+
+
+        }
+
+        public virtual void ChangeFileFormat(string format)
+        {
+            if (string.IsNullOrEmpty(format)) return;
+            _fileextension = format;
+            if (!File.Exists(FullPath)) CreateFile(); //проверить наличие по новоиу пути
+
+        }
+
+        public virtual void ChangeFileName(string name)
+        {
+            _filename = name;
+        }
+
+        public virtual void CreateFile()
+        {
+            if (!Directory.Exists(FolderPath)) Directory.CreateDirectory(FolderPath);
+            if (!File.Exists(FullPath)) File.Create(FullPath).Close();
+        }
+
+        public virtual void DeleteFile()
+        {
+            if (File.Exists(FullPath)) File.Delete(FullPath);
+        }
+
+        public virtual void EditFile(string text)
+        {
+
+            if (File.Exists(FullPath)) File.WriteAllText(FullPath, text);
+        }
+
+        public virtual void SelectFolder(string folder)
+        {
+            _folderpath = folder;
+        }
+    }
+}
---
+++ Lab10/PurpleJsonFileManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10.Purple
+{
+    public abstract class PurpleFileManager<T> : MyFileManager, ISerializer<T> where T : Lab9.Purple.Purple
+    {
+        public PurpleFileManager(string name) : base(name)
+        {
+        }
+
+        public PurpleFileManager(string name, string folder, string fileName, string extension = "txt") : base(name, folder, fileName, extension)
+        {
+        }
+
+        public override void ChangeFileExtension(string fileExtention)
+        {
+            if (File.Exists(FullPath))
+                base.ChangeFileExtension(fileExtention);
+        }
+        public override void EditFile(string fileContent)
+        {
+            if (File.Exists(FullPath))
+                base.EditFile(fileContent);
+        }
+        public abstract T Deserialize();
+
+        public abstract void Serialize(T purple);
+    }
+}
